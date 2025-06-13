d014183aa36bf56658ab1bb3d0271a0ff102f789
K Borger <borger151@gmail.com>
2025-06-13 17:06:29 +0000

    chore: reviewed both functions

--- element.sh
+++ element.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-MAIN_FUNCTION() {
+# MAIN_FUNCTION() {
   if [[ -z $1 ]]
   then
     echo Please provide an element as an argument.
@@ -10,7 +10,7 @@ MAIN_FUNCTION() {
   fi
 }
 
-PRINT_FUNCTION() {
+# PRINT_FUNCTION() {
   INPUT=$1
   if [[ ! $INPUT =~ ^[0-9]+$ ]]
   then
@@ -30,7 +30,7 @@ PRINT_FUNCTION() {
     MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
     BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
 
-    # echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   fi
 }
 
